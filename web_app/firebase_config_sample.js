de9d328787fef8319004a9902d7e5e1865118094
Can ÖZEL <161640033+canozzel@users.noreply.github.com>
2025-07-02 02:20:11 +0300

    Update firebase_config_sample.js

--- web_app/firebase_config_sample.js
+++ web_app/firebase_config_sample.js
@@ -1,1 +1,13 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project-id.firebaseapp.com",
+  databaseURL: "https://your-project-id.firebaseio.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "your-sender-id",
+  appId: "your-app-id"
+};
+
+// firebase.initializeApp(firebaseConfig);
+// const db = firebase.database();
 
